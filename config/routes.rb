a8c24b1a1017a26558540d28c2a4a0b0676cc645
BenediktusG <benediktus.gianto.jarod@gmail.com>
2025-12-18 10:11:21 +0000

    feat: add privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get "donate" => "home#donate"
   get "contact" => "home#contact"
+  get "privacy" => "home#privacy"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
